425d7b146dc2a9e854938597ee9e80f28391da8f
Vitalii Savitskii <savitskiy.vitaliy.m@gmail.com>
2026-04-19 12:56:56 +0200

    Encapsulate RoPE parameters in validated types

--- src/math.rs
+++ src/math.rs
@@ -3,9 +3,11 @@
 //! This module provides core mathematical primitives including matrix operations.
 
 mod matrix;
+mod rope;
 mod rmsnorm;
 mod softmax;
 
 pub use matrix::{Matrix, ShapeError};
+pub use rope::{apply_rope, compute_freqs, RopeFreqs, RopeHeadDim, RopeTheta};
 pub use rmsnorm::{rmsnorm, RmsNormError};
 pub use softmax::softmax;
